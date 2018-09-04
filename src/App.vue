55a1f869f0ccf98897a46977e58d13686411bab9
mattpilla <kaztalek@gmail.com>
2018-09-03 21:44:39 -0400

    ie unsupport

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
     <div id="app">
         <navbar></navbar>
+        <div v-if="ie" style="max-width: 900px; margin: 16px auto">
+            Instead of wasting time with browser compatibility, I decided to show this message if you're using Internet Explorer. The site doesn't work in IE. Please use another browser, not so that you can use this site, but for your own sanity.<br><br>
+            If you do see this message, you can message me on Twitter and let me know though. I can make the site <i>"usable"</i> with just a bit of effort, probably, if somebody out there truly wants this.
+        </div>
         <keep-alive>
             <router-view></router-view>
         </keep-alive>
@@ -13,6 +17,17 @@
     export default {
         components: {
             Navbar
+        },
+        data() {
+            return {
+                ie: false
+            }
+        },
+        created() {
+            let ua = window.navigator.userAgent;
+            if (ua.includes('MSIE ') || ua.includes('Trident/')) {
+                this.ie = true;
+            }
         }
     }
 </script>
